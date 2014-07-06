cd7b3395957afec6ffede7ed22ae0058a3fe2256
Jose Pedro <1jpsneto@gmail.com>
2014-07-06 13:13:53 -0300

    Refactorying float type

--- trabalho_da_carol.c
+++ trabalho_da_carol.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 
+FILE *fileTabela;
+
 typedef struct ldado{
-    int temperatura;
+    float temperatura;
     float pressao_sat;
     float vf;
     float vg;
@@ -19,68 +21,6 @@ typedef struct ldado{
     struct ldado *pro;
 } Ldado;
 
-
-
-FILE *fileTabela;
-
-Tdado *constroi(Tdado *raiz, int temperatura, float pressao_sat, float vf, float vg, float uf, float ufg, float ug, float hf, float hfg, float hg, float sf, float sfg, float sg){
-    if(raiz != NULL){
-        if(temperatura <= raiz->temperatura){
-            raiz->esq = constroi(raiz->esq, temperatura, pressao_sat, vf, vg, uf, ufg, ug, hf, hfg, hg, sf, sfg, sg);
-            //printf("\ncodigo <= raiz->codigo: %s\n",raiz->nome);
-        }
-        else{
-            raiz->dir = constroi(raiz->dir, temperatura, pressao_sat, vf, vg, uf, ufg, ug, hf, hfg, hg, sf, sfg, sg);
-            //printf("\nelse: %s\n",raiz->nome);
-        }        
-        return raiz;
-    }
-    else{
-        raiz = (Tdado *) malloc(sizeof(Tdado));
-        raiz->temperatura = temperatura;
-        raiz->pressao_sat = pressao_sat;
-        raiz->vf = vf;
-        raiz->vg = vg;
-        raiz->uf = uf;
-        raiz->ufg = ufg;
-        raiz->ug = ug;
-        raiz->hf = hf;
-        raiz->hfg = hfg;
-        raiz->hg = hg;
-        raiz->sf = sf;
-        raiz->sfg = sfg;
-        raiz->sg = sg;
-        raiz->esq = NULL;
-        raiz->dir = NULL;
-        return raiz;
-    }
-}
-
-void mostrar_valores_decrescente_temperatura(Tdado *raiz){
-      if(raiz != NULL){
-          mostrar_valores_decrescente_temperatura(raiz->esq);
-          mostrar_valores_decrescente_temperatura(raiz->dir);
-          printf("%d %f %f %f %f %f %f %f %f %f %f %f %f\n", raiz->temperatura, raiz->pressao_sat, raiz->vf, raiz->vg, raiz->uf, raiz->ufg, raiz->ug, raiz->hf, raiz->hfg, raiz->hg, raiz->sf, raiz->sfg, raiz->sg);  
-
-      }
-}
-
-Tdado *pesquisa_temperatura(Tdado *raiz, int temperatura){
-    if (raiz != NULL){
-        if (temperatura < raiz->temperatura){
-            pesquisa_temperatura(raiz->esq, temperatura);         
-        }
-        else if (temperatura > raiz->temperatura){
-            pesquisa_temperatura(raiz->dir, temperatura);
-        }     
-        else if (temperatura == raiz->temperatura){
-            return raiz;
-        }
-        else {
-            return NULL;
-        }
-    }
-}
 
 Ldado *adicionar_dado_lista(Ldado *lista, int temperatura, float pressao_sat, float vf, float vg, float uf, float ufg, float ug, float hf, float hfg, float hg, float sf, float sfg, float sg){
     if (lista != NULL){
@@ -109,7 +49,7 @@ Ldado *adicionar_dado_lista(Ldado *lista, int temperatura, float pressao_sat, fl
 void listar_elementos(Ldado *lista){
     if (lista != NULL)
     {
-        printf("%d %f %f %f %f %f %f %f %f %f %f %f %f\n", lista->temperatura, lista->pressao_sat, lista->vf, lista->vg, lista->uf, lista->ufg, lista->ug, lista->hf, lista->hfg, lista->hg, lista->sf, lista->sfg, lista->sg);
+        printf("%f %f %f %f %f %f %f %f %f %f %f %f %f\n", lista->temperatura, lista->pressao_sat, lista->vf, lista->vg, lista->uf, lista->ufg, lista->ug, lista->hf, lista->hfg, lista->hg, lista->sf, lista->sfg, lista->sg);
         listar_elementos(lista->pro);
     }
 }
@@ -154,23 +94,6 @@ Ldado *interpola_pesquisa_temperatura(Ldado *lista, int temperatura){
     return dados_consulta;
 }
 
-void mostrar_resultado_pesquisa_arvore(Tdado *dados_consulta){
-    printf("\n\tDados da consulta:\n");
-    printf("\t\tTemperatura: %d graus Celcius\n", dados_consulta->temperatura); 
-    printf("\t\tPressao de Saturacao: %f kPa\n", dados_consulta->pressao_sat); 
-    printf("\t\tVolume Especifico Saturado Liquido: %f m^3/kg\n", dados_consulta->vf); 
-    printf("\t\tVolume Especifico Saturado Vapor: %f m^3/kg\n", dados_consulta->vg);
-    printf("\t\tEnergia Interna Saturado Liquido: %f kJ/kg\n", dados_consulta->uf); 
-    printf("\t\tEnergia Interna Evaporacao: %f kJ/kg\n", dados_consulta->ufg);
-    printf("\t\tEnergia Interna Saturado Vapor: %f kJ/kg\n", dados_consulta->ug);  
-    printf("\t\tEntalpia Saturado Liquido: %f kJ/kg\n", dados_consulta->hf);
-    printf("\t\tEntalpia Evaporacao: %f kJ/kg\n", dados_consulta->hfg);
-    printf("\t\tEntalpia Saturado Vapor: %f kJ/kg\n", dados_consulta->hg);
-    printf("\t\tEntropia Saturado Liquido: %f kJ/kg . K\n", dados_consulta->sf);
-    printf("\t\tEntropia Evaporacao: %f kJ/kg . K\n",dados_consulta->sfg);
-    printf("\t\tEntropia Saturado Vapor: %f kJ/kg . K\n", dados_consulta->sg);
-}
-
 void mostrar_resultado_pesquisa_lista(Ldado *dados_consulta){
     printf("\n\tDados da consulta:\n");
     printf("\t\tTemperatura: %d graus Celcius\n", dados_consulta->temperatura); 
@@ -188,19 +111,7 @@ void mostrar_resultado_pesquisa_lista(Ldado *dados_consulta){
     printf("\t\tEntropia Saturado Vapor: %f kJ/kg . K\n", dados_consulta->sg);
 }
 
-void pesquisa_temperatura_saturado(){
-
-    Tdado *raiz = NULL;
-    Ldado *lista = NULL;
-
-    //Abrindo arquivo
-    fileTabela = fopen("saturado-entrada-temperatura.txt","r");
-    if (fileTabela == NULL)
-    {
-        printf("\n%s\n", "Arquivo nao encontrado");
-    }
-
-    //Contando quantidade de linhas no arquivo
+int quantidade_linhas(FILE *fileTabela){
     char caractere = getc(fileTabela);
     int linhas = 0;
     while(caractere != EOF){
@@ -211,16 +122,25 @@ void pesquisa_temperatura_saturado(){
         else 
             caractere = getc(fileTabela);
     }
+    return linhas;
+}
 
-    //resetando o ponteiro do arquivo
-    fclose(fileTabela);
-    fileTabela = fopen("saturado-entrada-temperatura.txt","r");
+FILE *abrir_arquivo(FILE *fileTabela, char *nome_arquivo){
+    fileTabela = fopen(nome_arquivo,"r");
     if (fileTabela == NULL)
     {
         printf("\n%s\n", "Arquivo nao encontrado");
     }
+    return fileTabela; 
+}
 
-    //Construindo a arvore
+FILE *resetar_tabela(FILE *fileTabela, char *nome_arquivo){
+    fclose(fileTabela);
+    fileTabela = abrir_arquivo(fileTabela, nome_arquivo);
+    return fileTabela;
+}
+
+Ldado *extrair_dados(FILE *fileTabela, int linhas, Ldado *lista){
     int i;
     int temperatura;
     float pressao_sat;
@@ -241,17 +161,35 @@ void pesquisa_temperatura_saturado(){
             &vf, &vg, &uf, &ufg, &ug, &hf, &hfg, &hg, &sf, &sfg, &sg);
         //printf("%d %f %f %f %f %f %f %f %f %f %f %f %f\n", temperatura, pressao_sat, vf, vg, uf, ufg, ug, hf, hfg, hg, sf, sfg, sg);
         //Copiando os dados para as tabelas
-        lista = adicionar_dado_lista(lista, temperatura, pressao_sat, vf, vg, uf, ufg, ug, hf, hfg, hg, sf, sfg, sg);
-        raiz = constroi(raiz, temperatura, pressao_sat, vf, vg, uf, ufg, ug, hf, hfg, hg, sf, sfg, sg);
-        //printf("\n%s\t%s\t%s\t%s\t%s\n", videos_codigo[i], videos_titulo[i], videos_midia[i], videos_preco[i], videos_genero[i]);
+        lista = adicionar_dado_lista(lista, (float) temperatura, pressao_sat, vf, vg, uf, ufg, ug, hf, hfg, hg, sf, sfg, sg);
     }
+    return lista;
+}
 
-    //mostrar_valores_decrescente_temperatura(raiz);
+void pesquisa_temperatura_saturado(){
 
-    //listar_elementos(lista);
+    //iniciando vairiáveis para pesquisa
+    Ldado *lista = NULL;
+    char *nome_arquivo = "saturado-entrada-temperatura.txt";
+
+    //Abrindo arquivo
+    fileTabela = abrir_arquivo(fileTabela, nome_arquivo);
+
+    //Contando quantidade de linhas no arquivo
+    int linhas = quantidade_linhas(fileTabela); 
+
+
+    //resetando o ponteiro do arquivo
+    fileTabela = resetar_tabela(fileTabela, nome_arquivo);
+
+
+    //Construir lista
+    lista = extrair_dados(fileTabela, linhas, lista);
+
+    listar_elementos(lista);
 
     //Pesquisando Temperatura
-    float temperatura_consulta;
+    /*float temperatura_consulta;
     printf("\n\n\tDigite a temperatura em graus celcius: ");
     scanf("%f",&temperatura_consulta);
 
@@ -268,7 +206,7 @@ void pesquisa_temperatura_saturado(){
     else{
         mostrar_resultado_pesquisa_arvore(dados_consulta);
     }
-
+*/
 
     
     fclose(fileTabela);
